07e1d886a63514956597cc6381e268a376a5e3ec
AliAldayoub <ali.dy111234@gmail.com>
2023-05-09 10:32:05 -0500

    edait login&signup  route

--- controllers/auth.js
+++ controllers/auth.js
@@ -80,7 +80,7 @@ exports.signup = async (req, res, next) => {
 
 		await user.save();
 		const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
-		res.cookie('token', token, { httpOnly: true });
+		res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'lax' });
 		return res.status(201).json({ message: 'User created. Check your email for activation code.', token, user });
 	} catch (error) {
 		next(error);
@@ -107,7 +107,7 @@ exports.login = async (req, res, next) => {
 		}
 
 		const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
-		res.cookie('token', token, { httpOnly: true });
+		res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'lax' });
 		res.json({ token });
 	} catch (error) {
 		next(error);
