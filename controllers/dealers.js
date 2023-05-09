f20c412aba75d67fd72cd98e712365d50067e760
AliAldayoub <ali.dy111234@gmail.com>
2023-05-09 08:09:00 -0500

    add route to insert dealer

--- controllers/dealers.js
+++ controllers/dealers.js
@@ -4,16 +4,20 @@ exports.getAllDealers = async (req, res, next) => {
 	try {
 		const perPage = 10;
 		const page = req.query.page || 1;
-		const dealers = await Dealer.find().skip(perPage * page - perPage).limit(perPage);
-		const count = await Dealer.countDocuments(); // count documents for user id
-		const totalPages = Math.ceil(count / perPage);
-		res.status(201).json({
-			success: true,
-			message: 'All dealers Ae retrieved successfully',
-			data: dealers,
-			currentPage: page,
-			totalPages: totalPages
-		});
+		const count = await Dealer.countDocuments();
+		if (count == 0) {
+			res.status(200).json({ message: 'no dealer in site for now' });
+		} else {
+			const dealers = await Dealer.find().skip(perPage * page - perPage).limit(perPage);
+			const totalPages = Math.ceil(count / perPage);
+			res.status(201).json({
+				success: true,
+				message: 'All dealers Ae retrieved successfully',
+				data: dealers,
+				currentPage: page,
+				totalPages: totalPages
+			});
+		}
 	} catch (error) {
 		next(error);
 	}
