2328e27fd87e6a6c3bbf9a71523f7050d15b4d87
AliAldayoub <ali.dy111234@gmail.com>
2023-05-09 08:04:52 -0500

    add route to insert dealer

--- routes/dealers.js
+++ routes/dealers.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const dealersController = require('../controllers/dealers');
 router.get('/getAllDealers', dealersController.getAllDealers);
-router.get(
+router.post(
 	'/addDealer',
 	authMiddleware.authenticateUser,
 	authMiddleware.authenticateAdmin,
