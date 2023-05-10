72ddf40ab762d437c555396da43b13b2c5c4f3a3
AliAldayoub <ali.dy111234@gmail.com>
2023-05-10 10:00:20 -0500

    edit login

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ app.use(
 		credentials: true
 	})
 );
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
