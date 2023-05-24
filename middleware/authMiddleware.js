a9133ef76ba770c7eba4b12f30057a07c6e86051
AliAldayoub <ali.dy111234@gmail.com>
2023-05-24 04:58:16 -0500

    edit auth middleware logging

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,9 +3,9 @@ const User = require('../models/user');
 
 exports.authenticateUser = async (req, res, next) => {
 	try {
-		const token = req.cookies.token;
+		// const token = req.cookies.token;
+		const token = req.headers.authorization.split(' ')[1];
 		console.log(token);
-		// const token = req.headers.authorization.split(' ')[1];
 		if (!token) {
 			return res.status(401).json({ message: 'no token you can not access this route' });
 		} else {
